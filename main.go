7c8f4f86c67200207e95d2fecf0d8c2fec17c3ee
Andris Buscariolli <buskarion@gmail.com>
2025-04-21 13:09:37 -0300

    feat: status code validation

--- main.go
+++ main.go
@@ -21,6 +21,24 @@ func main() {
 		log.Fatalf("Failed to read response body: %v", err)
 	}
 
-	fmt.Println("Raw respnse:")
-	fmt.Println(string(body))
+	fmt.Printf("Response Status Code: %d", int(resp.StatusCode))
+	if resp.StatusCode == http.StatusOK {
+		fmt.Println("\nRaw response:")
+		fmt.Println(string(body))
+	} else if resp.StatusCode == http.StatusNotFound {
+		fmt.Println("\nThe required resource does not exists.")
+		fmt.Printf("Error details: %v", body)
+	} else if resp.StatusCode == http.StatusUnauthorized {
+		fmt.Println("\nUnauthorized. Access denied.")
+		fmt.Printf("Error details: %v", body)
+	} else if resp.StatusCode == http.StatusBadRequest {
+		fmt.Println("\nBad Request. The server does not understand the request due to syntax error.")
+		fmt.Printf("Error details: %v", body)
+	} else if resp.StatusCode == http.StatusInternalServerError {
+		fmt.Println("\nInternal Server Error. The server could not handle the request. Try again later.")
+		fmt.Printf("Error details: %v", body)
+	} else {
+		fmt.Println("\nUnexpected Status Code")
+		fmt.Printf("Error details: %v", body)
+	}
 }
